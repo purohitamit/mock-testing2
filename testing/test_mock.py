340e0857d7b64294d98d815166dc8733abe909b5
Ubuntu <azureuser@myVM.5qh0wf5nsbruhlksmk54necr3c.zx.internal.cloudapp.net>
2021-11-22 18:32:15 +0000

    uploading to github

---
+++ testing/test_mock.py
@@ -0,0 +1,44 @@
+from unittest.mock import patch
+from flask import url_for
+from flask_testing import TestCase
+import requests_mock
+
+from application import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestResponse(TestBase):
+
+    def test_football(self):
+    # We will mock a response of 1 and test that we get football returned.
+        with requests_mock.Mocker() as m:
+            m.get('http://api:5000/get/number', text='1')
+            m.get('http://api:5000/get/letter', text='a')
+
+            response = self.client.get(url_for('sport'))
+            self.assertIn(b'Football', response.data)
+    def test_badminton(self):
+    # We will mock a response of 1 and test that we get football returned.
+        with requests_mock.Mocker() as m:
+            m.get('http://api:5000/get/number', text='1')
+            m.get('http://api:5000/get/letter', text='b')
+            response = self.client.get(url_for('sport'))
+            self.assertIn(b'Badminton', response.data)
+    def test_hockey(self):
+    # We will mock a response of 1 and test that we get football returned.
+       with requests_mock.Mocker() as m:
+            m.get('http://api:5000/get/number', text='1')
+            m.get('http://api:5000/get/letter', text='c')
+            response = self.client.get(url_for('sport'))
+            self.assertIn(b'Hockey', response.data)
+    def test_boxing(self):
+    # We will mock a response of 1 and test that we get football returned.
+        with requests_mock.Mocker() as m:
+            m.get('http://api:5000/get/number', text='1')
+            m.get('http://api:5000/get/letter', text='d')
+
+            response = self.client.get(url_for('sport'))
+            self.assertIn(b'Boxing', response.data)
+    
